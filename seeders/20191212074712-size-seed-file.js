c5c6512d337c0ffd98a7e57e1795e45fdabc1ed1
Eason Lin <uu9924079@gmail.com>
2019-12-13 21:33:28 +0800

    fix: model naming bugs

--- seeders/20191212074712-size-seed-file.js
+++ seeders/20191212074712-size-seed-file.js
@@ -3,7 +3,7 @@ const faker = require('faker')
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
-      'ProductSizes',
+      'Sizes',
       Array.from({ length: 3 }).map((item, index) => ({
         id: index + 1,
         size: ['S', 'M', 'L'][index],
@@ -15,6 +15,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('ProductSizes', null, {})
+    return queryInterface.bulkDelete('Sizes', null, {})
   }
 }
