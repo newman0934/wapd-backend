152c469ad1f665813290e103bff383f2bacfe066
Eason Lin <uu9924079@gmail.com>
2019-12-13 21:54:03 +0800

    chore: add /products, /products/:id and admins/products, admins/prdocuts/:id routes

--- routes/apis.js
+++ routes/apis.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
 const productController = require('../controllers/api/productController')
+const adminController = require('../controllers/api/adminController')
 
-router.get('/', productController.getProducts)
+router.get('/', (req, res) => res.redirect('/api/products'))
+router.get('/products', productController.getProducts)
+router.get('/products/:id', productController.getProduct)
+
+router.get('/admins/products', adminController.getProducts)
+router.get('/admins/products/:id', adminController.getProduct)
 
 module.exports = router
