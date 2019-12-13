3dd9028ad18c289649a97346ce4ee7391b780a13
Eason Lin <uu9924079@gmail.com>
2019-12-13 21:28:19 +0800

    fix: delete console statement

--- controllers/api/productController.js
+++ controllers/api/productController.js
@@ -3,7 +3,6 @@ const productService = require('../../services/productService')
 const productController = {
   getProducts: (req, res) => {
     productService.getProducts(req, res, data => {
-      console.log('data: ' + data)
       return res.json(data)
     })
   }
