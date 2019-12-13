bc382d345416374d2f2515d6e1d9390149f8dea9
Eason Lin <uu9924079@gmail.com>
2019-12-13 23:56:07 +0800

    refactor: getProduct logic

--- services/productService.js
+++ services/productService.js
@@ -1,29 +1,51 @@
 const db = require('../models')
 const Product = db.Product
 const Category = db.Category
+const ProductStatus = db.ProductStatus
+const Color = db.Color
+const Size = db.Size
 
 const productService = {
   getProducts: async (req, res, callback) => {
-    let whereQuery = {}
-    let categoryId = ''
-    if (req.query.categoryId) {
-      categoryId = Number(req.query.categoryId)
-      whereQuery['CategoryId'] = categoryId
-    }
+    // let whereQuery = {}
+    // if (req.query.categoryId) {
+    //   categoryId = Number(req.query.categoryId)
+    //   whereQuery['CategoryId'] = categoryId
+    // }
 
-    const productResult = await Product.findAll({
-      include: Category,
-      where: whereQuery
+    let categoryId = ''
+
+    const productResult = await ProductStatus.findAll({
+      include: [Product, Color, Size]
+      // where: whereQuery
     })
     const categories = await Category.findAll()
 
-    const data = productResult.map(r => ({
-      ...r.dataValues
+    let products = productResult.map(data => ({
+      id: data.dataValues.id,
+      name: data.dataValues.Product.name,
+      description: data.dataValues.Product.description,
+      status: data.dataValues.Product.status,
+      sales: data.dataValues.sales,
+      stock: data.dataValues.stock,
+      cost: data.dataValues.cost,
+      price: data.dataValues.price,
+      color: data.dataValues.Color.color,
+      size: data.dataValues.Size.size,
+      CategoryId: data.dataValues.Product.CategoryId,
+      ProductId: data.dataValues.ProductId,
+      createdAt: data.dataValues.Product.createdAt,
+      updatedAt: data.dataValues.Product.updatedAt
     }))
+    // 使用者點擊類別，回傳對應商品
+    if (req.query.categoryId) {
+      products = products.filter(d => d.CategoryId === +req.query.categoryId)
+    }
+
     return callback({
-      products: data,
+      products,
       categories,
-      categoryId
+      categoryId: +req.query.categoryId
     })
   },
   getProduct: async (req, res, callback) => {
