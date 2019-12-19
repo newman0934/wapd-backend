2f0be25b40c50f49a87e12fddc44d11176b32aae
Eason Lin <uu9924079@gmail.com>
2019-12-19 17:23:40 +0800

    fix: update seeders

--- seeders/20191212083150-order-item-seed-file.js
+++ seeders/20191212083150-order-item-seed-file.js
@@ -6,8 +6,9 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
       'OrderItems',
-      Array.from({ length: 10 }).map((item, index) => ({
-        OrderId: Math.floor(Math.random() * 2) + 1,
+      Array.from({ length: 20 }).map((item, index) => ({
+        id: index + 1,
+        OrderId: Math.floor(index / 2) + 1,
         ProductId: Math.floor(Math.random() * 10) + 1,
         sell_price: Math.floor(Math.random() * 500) + 1,
         quantity: Math.floor(Math.random() * 10) + 1,
