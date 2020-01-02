a034011add0fe236d10dcc3b3f9f2436717c7975
Eason Lin <uu9924079@gmail.com>
2020-01-02 11:53:15 +0800

    chore: install swagger and try api doc

--- controllers/api/productController.js
+++ controllers/api/productController.js
@@ -1,15 +1,43 @@
 const productService = require('../../services/productService')
 
 const productController = {
+  /**
+   * @swagger
+   * /api/products:
+   *    get:
+   *      description: This should return status-on products
+   *      responses:
+   *         200:
+   *           description: An array of products
+   */
   getProducts: (req, res) => {
     productService.getProducts(req, res, data => {
-      return res.json(data)
+      return res.status(200).json(data)
     })
   },
-
+  /**
+   * @swagger
+   * /api/products/{id}:
+   *    get:
+   *      description: Find status-on Product by ID
+   *      operationId: getProductById
+   *      parameters:
+   *      - in: path
+   *        name: id
+   *        description: return a status-on product with images, size, color and categories
+   *        required: true
+   *        schema:
+   *          type: integer
+   *          format: int64
+   *      security:
+   *        - bearerAuth: []
+   *      responses:
+   *         200:
+   *           description: a product found by id
+   */
   getProduct: (req, res) => {
     productService.getProduct(req, res, data => {
-      return res.json(data)
+      return res.status(200).json(data)
     })
   },
 
