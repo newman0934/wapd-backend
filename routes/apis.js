f424cb41592f76ad877ac52e79a4054021d5eaf6
Eason Lin <uu9924079@gmail.com>
2020-01-13 12:52:47 +0800

    fix: spgateway url

--- routes/apis.js
+++ routes/apis.js
@@ -252,7 +252,7 @@ router.post(
   orderController.postCheckout
 )
 router.get(`/orders/:id/payment`, authenticated, orderController.getPayment)
-router.post(`/spgateway/callback`, orderController.spgatewayCallback)
+router.post(`/spgateway/ReturnURL`, orderController.spgatewayCallback)
 router.get(
   `/users/paymentcomplete`,
   authenticated,
