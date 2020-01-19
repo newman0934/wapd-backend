c6133941f5868ae3e162870a98eb35f14d868344
Eason Lin <uu9924079@gmail.com>
2020-01-19 13:09:40 +0800

    travis config

--- config/config.js
+++ config/config.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 module.exports = {
   development: {
     username: 'root',
@@ -7,8 +11,8 @@ module.exports = {
     dialect: 'mysql'
   },
   test: {
-    username: process.env.NODE_ENV === 'production' ? 'travis' : 'root',
-    password: process.env.NODE_ENV === 'production' ? '' : 'password',
+    username: process.env.CONFIG_USER_NAME,
+    password: process.env.CONFIG_USER_PASS,
     database: 'wapd_backend_test',
     host: '127.0.0.1',
     dialect: 'mysql',
